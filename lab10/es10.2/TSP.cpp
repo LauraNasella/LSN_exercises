15850f97e9a7c5f36c18d93b6bc04b6de467ec00
LauraNasella <nasella.laura@gmail.com>
2020-09-22 11:25:19 +0200

    lab10 finito

--- lab10/es10.2/TSP.cpp
+++ lab10/es10.2/TSP.cpp
@@ -13,20 +13,16 @@ void City::set_pos(int & posiz){
 Individuo::Individuo(int number, const City & inizio, vector<City>::iterator it1, vector<City>::iterator it2) : N(number), partenza(inizio) {
 	for (auto it = it1; it !=it2; it++) {
 		percorso.push_back(*it);
-		//cout << (it)->getx() << " " << (it)->gety() << endl;
-		//cout << percorso[0].getx() << " " << percorso[0].gety() << endl;
 	}
 }
 
 //Scelgo casualmente due delle ultime 31 città (da 0 a 30) e le scambio. Pesco da 0 a 31 così la parte intera sarà da 0 a 30 e sono 31 valori.
 void Individuo::mutation_swap(Random & rnd){
 	int i = int(rnd.Rannyu(0,31));
-	//cout << i << " swap" << endl;
 	int j=0;
 	do{
        		j = int(rnd.Rannyu(0,31));
      	}while(i==j);
-	//cout << j << endl;
 	swap(this->percorso[i],this->percorso[j]);
 	rnd.SaveSeed();
 }
@@ -34,7 +30,6 @@ void Individuo::mutation_swap(Random & rnd){
 //Scelgo casualmente un numero n<N-1 e traslo tutte le città (tranne la prima) di n.
 void Individuo::mutation_shift(Random & rnd){
 	int n = int(rnd.Rannyu(0,30));
-	//cout << n << " shift" << endl;
 	vector<City> copia; 
 	for (auto it = this->percorso.begin()+int(this->percorso.size()-n); it != this->percorso.end(); it++) {
 		copia.push_back(*it);
@@ -53,10 +48,8 @@ void Individuo::mutation_shift(Random & rnd){
 //Scelgo casualmente un numero m<N/2 e permuto m città contigue con altre m città contigue.
 void Individuo::mutation_permutation(Random & rnd){
 	int m = int(rnd.Rannyu(0,15));
-	//cout << m << " perm" << endl;
 	int i = int(rnd.Rannyu(0,31)); //scelgo una delle città casualmente
-	//cout << i << endl;
-
+	
 	int indice_piu=0;
 	if((i+m-1)>30){ //cioè esce a destra quindi devo usare le pbc
 		indice_piu = (i+m-1)-30-1;
@@ -85,8 +78,6 @@ void Individuo::mutation_permutation(Random & rnd){
      		}while((j>=indice_meno)&&(j<=indice_piu));
 	}
 	
-	//cout << j << endl;
-	
 	vector<City> copia; 
 	for (auto it = this->percorso.begin(); it != this->percorso.end(); it++) {
 		copia.push_back(*it);
@@ -111,9 +102,7 @@ void Individuo::mutation_permutation(Random & rnd){
 //Scelgo casualmente un numero m<=N-1 e inverto m città dopo la i-esima scelta casualmente.
 void Individuo::mutation_inversion(Random & rnd){
 	int m = int(rnd.Rannyu(0,31));
-	//cout << m << " inv" << endl;
 	int i = int(rnd.Rannyu(0,31)); //scelgo una delle città casualmente
-	//cout << i << " partenza" << endl;
 	int j = i+m-1;
 	if(j<=30){
 		int l = int(m/2);
@@ -155,11 +144,9 @@ double Individuo::L1() const{
   	}
 	
 	double d_partenza_0 = sqrt((this->partenza.getx()-this->percorso[0].getx())*(this->partenza.getx()-this->percorso[0].getx())+(this->partenza.gety()-this->percorso[0].gety())*(this->partenza.gety()-this->percorso[0].gety()));
-	//cout << d_partenza_0 << endl;
 
 	double d_fine_partenza = sqrt((this->partenza.getx()-this->percorso[30].getx())*(this->partenza.getx()-this->percorso[30].getx())+(this->partenza.gety()-this->percorso[30].gety())*(this->partenza.gety()-this->percorso[30].gety()));
-	//cout << d_fine_partenza << endl;
-
+	
 	lunghezza2 = lunghezza2 + d_partenza_0 + d_fine_partenza;
   
 	return lunghezza2;
@@ -223,28 +210,23 @@ Popolazione::Popolazione(int number, Random & rnd, Individuo primo) : N_pop(numb
 	}
 	for (int i=0; i<24; i++){ //così ciclo 24 volte (da 0 a 23) e in ogni ciclo uso le 4 mutazioni. In totale creo così altre 96 mutazioni che con le altre 4 arriva a 100	
 		mutato.mutation_swap(rnd);
-		//cout << "ciao1" << endl;
 		if(mutato.check() == false){
 			pop.push_back(mutato);
 		}
 		mutato.mutation_shift(rnd);
-		//cout << "ciao2" << endl;
 		if(mutato.check() == false){
 			pop.push_back(mutato);
 		}
 		mutato.mutation_permutation(rnd);
-		//cout << "ciao3" << endl;
 		if(mutato.check() == false){
 			pop.push_back(mutato);
 		}
 		mutato.mutation_inversion(rnd);
-		//cout << "ciao4" << endl;
 		if(mutato.check() == false){
 			pop.push_back(mutato);
 		}
 		
 	}
-	//cout << pop.size() << endl;
 	rnd.SaveSeed();
 }
 
@@ -256,9 +238,7 @@ void Popolazione::sort_L1() {
 //Selezione
 int Popolazione::selezione(Random & rnd) {
 	int N = this->N_pop;
-	//cout << "N " << N << endl;
 	double r = rnd.Rannyu();
-	//cout << r << endl;
 	double p = 3.;
 	int j = int(N*pow(r,p));
 	rnd.SaveSeed();
@@ -270,35 +250,7 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 	Individuo madre(this->pop[k]);
 	Individuo padre(this->pop[l]);
 
-	//cout << k << endl;
-	//cout << l << endl;
-	//NON devo eliminarli
-	/*
-	if(k>l){
-		this->pop.erase(pop.begin()+k);
-		this->pop.erase(pop.begin()+l);
-	}
-	else{
-		this->pop.erase(pop.begin()+l);
-		this->pop.erase(pop.begin()+k);
-	}
-	*/
-	/*
-	cout << "madre" << endl;
-	cout << madre.L1() << endl;
-	for (auto i : madre.get_percorso()){
-		cout << i.getx() << " " << i.gety() << endl;
-	}
-	
-	cout << "padre" << endl;
-	cout << padre.L1() << endl;
-	for (auto i : padre.get_percorso()){
-		cout << i.getx() << " " << i.gety() << endl;
-	}
-	*/
 	int cut = int(rnd.Rannyu(1,30)); //scelgo una delle città casualmente, tra le 31 che ci sono nel percorso, la partenza non la considero tanto è sempre la stessa. Fino a cut escluso conservo
-	//cut = int(rnd.Rannyu(1,30));
-	//cout << "cut " << cut << endl;
 	vector<City> percorso_f1;
 	vector<City> percorso_f2;
 
@@ -310,16 +262,7 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 	for (int i=0; i<cut; i++) {
 		percorso_f2.push_back(padre.get_percorso()[i]);
 	}
-	/*
-	cout << "f1" << endl;
-	for (int i=0; i<percorso_f1.size(); i++){
-		cout << percorso_f1[i].getx() << " " << percorso_f1[i].gety() << endl;
-	}
-	cout << "f2" << endl; 
-	for (int i=0; i<percorso_f2.size(); i++){
-		cout << percorso_f2[i].getx() << " " << percorso_f2[i].gety() << endl;
-	}
-	*/
+	
 	vector<City> after_cut1;
 	vector<City> after_cut2;
 
@@ -330,65 +273,35 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 	}
 
 	int dim = after_cut1.size(); //tanto è uguale
-	/*
-	cout<<"after1" << endl;
-	for (int i=0; i<after_cut1.size(); i++){
-		cout << after_cut1[i].getx() << " " << after_cut1[i].gety() << endl;
-	}
-	cout<<"after2" << endl; 
-	for (int i=0; i<after_cut2.size(); i++){
-		cout << after_cut2[i].getx() << " " << after_cut2[i].gety() << endl;
-	}
-	cout << "pos1" << endl;
-	*/
+	
 	//scorro l'altro vettore per trovare le posizioni corrispondenti
 	for (int i=0; i<dim; i++) {
 		for(int j=0; j<31; j++) {//devo scorrere tutto il padre per cercare
 			if(after_cut1[i].getx() == padre.get_percorso()[j].getx()){
-				//cout << j << endl;
 				int posiz = j;
 				after_cut1[i].set_pos(posiz);
-				//cout << (after_cut1[i].get_pos()) << endl;
 			}
 			else{
 			}
 		}
 	}
 	dim = after_cut2.size();
-	//cout << "pos2" << endl;
 	for (int i=0; i<dim; i++) {
 		for(int j=0; j<31; j++) {//devo scorrere tutto la madre per cercare
 			if(after_cut2[i].getx() == madre.get_percorso()[j].getx()){
-				//cout << j << endl;
 				int posiz=j;
 				after_cut2[i].set_pos(posiz);
-				//cout << (after_cut2[i].get_pos()) << endl;
 			}
 			else{
 			}
 		}
 	}
-	//cout << endl;
-
-	dim = after_cut2.size();
-	/*cout << "pos1" << endl;
-	for (int i=0; i<dim; i++) {
-		cout << (after_cut1[i].get_pos()) << endl;
-	}
-	*/
-	/*
-	cout << "pos2" << endl;
 	
-	for (int i=0; i<dim; i++) {
-		cout << (after_cut2[i].get_pos()) << endl;
-	}
-	*/
-	//cout << "post" << endl;
+	dim = after_cut2.size();
 	
 	vector<City> appoggio1;
 	vector<City> appoggio2;
 
-	//cout << after_cut1.size() <<endl;
 	for(int i=0; i<after_cut1.size()-1; i++){
 	int min = i;
 		for(int j=i+1; j<after_cut1.size(); j++){
@@ -396,34 +309,23 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
     			min = j;
 			}
 		}
-	//cout << "min" << min << " " << after_cut1[min].getx() << endl;
+
 	appoggio1.push_back(after_cut1[min]);
 	after_cut1.erase(after_cut1.begin()+min);
-	/*
-	for (int y=0; y<after_cut1.size(); y++) {
-		cout << (after_cut1[y].get_pos()) << " " << (after_cut1[y].getx()) << endl;
-	}
-	*/
+	
 	i=i-1;
 	}
 
 	appoggio1.push_back(after_cut1[0]);
 	auto it1 = appoggio1.begin();
 	auto it2 = appoggio1.end();
-	//cout << "appo1" << endl;
+
 	for(auto it=it1; it!=it2;it++){
 		int posiz = 1;
 		(*it).set_pos(posiz);
-		//cout << (*it).get_pos() << endl;
-		//cout << (*it).getx() << " " <<(*it).gety() << endl;
 		percorso_f1.push_back(*it);
 	}
-	/*cout << "percorso1" << endl;
-	for (int i=0; i<percorso_f1.size(); i++){
-		cout << percorso_f1[i].getx() << " " << percorso_f1[i].gety() << endl;
-	}
-	*/
-	//cout << endl;
+
 	for(int i=0; i<after_cut2.size()-1; i++){
 	int min = i;
 		for(int j=i+1; j<after_cut2.size(); j++){
@@ -431,14 +333,10 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
     			min = j;
 			}
 		}
-	//cout << "min" << min << " " << after_cut2[min].getx() << endl;
+
 	appoggio2.push_back(after_cut2[min]);
 	after_cut2.erase(after_cut2.begin()+min);
-	/*
-	for (int y=0; y<after_cut2.size(); y++) {
-		cout << (after_cut2[y].get_pos()) << " " << (after_cut2[y].getx()) << endl;
-	}
-	*/
+
 	i=i-1;
 	}
 	
@@ -449,17 +347,10 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 	for(auto it=it1; it!=it2;it++){
 		int posiz = 1;
 		(*it).set_pos(posiz);
-		//cout << (*it).get_pos() << endl;
-		//cout << (*it).getx() << " " <<(*it).gety() << endl;
+
 		percorso_f2.push_back(*it);
 	}
-	/*
-	cout << "percorso2" << endl;
-	for (int i=0; i<percorso_f2.size(); i++){
-		cout << percorso_f2[i].getx() << " " << percorso_f2[i].gety() << endl;
-	}
-	*/
-	//cout << endl;
+
 	dim = percorso_f1.size(); //tanto è uguale
 	if (dim!=31){
 		cout << "ERRORE!!" << endl;
@@ -472,7 +363,6 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 	//Ora sostituisco la madre e il padre con i due figli
 	int getN = madre.get_N();
 	City getpart = madre.get_partenza();
-	//cout << getpart.getx() << endl;
 
 	it1 = percorso_f1.begin();
 	it2 = percorso_f1.end();
@@ -480,29 +370,18 @@ void Popolazione::crossover(Random & rnd, int k, int l) {
 
 	
 	City getpart2 = padre.get_partenza();
-	//cout << getpart2.getx() << endl;
-	//cout << "figliofinale1" << endl;
-	/*
-	for (int i=0; i<figlio1.get_percorso().size(); i++){
-		cout << figlio1.get_percorso()[i].getx() << " " << figlio1.get_percorso()[i].gety() << endl;
-	}
-	*/
+	
 	it1 = percorso_f2.begin();
 	it2 = percorso_f2.end();
 	Individuo figlio2(getN, getpart, it1,it2);
-	//cout << "figliofinale2" << endl;
-	/*
-	for (int i=0; i<figlio2.get_percorso().size(); i++){
-		cout << figlio2.get_percorso()[i].getx() << " " << figlio2.get_percorso()[i].gety() << endl;
-	}
-	*/
+	
 	if(figlio1.check() == false){
 			this->pop.push_back(figlio1);
 	}
 	if(figlio2.check() == false){
 			this->pop.push_back(figlio2);
 	}
-	//cout << this->pop.size() << endl; /*****/
+	
 	rnd.SaveSeed();
 	percorso_f1.clear();
 	percorso_f2.clear();
@@ -518,9 +397,7 @@ void Popolazione::new_generazione(Random & rnd) {
 	double prob_mutazione = 0.09;
 	double prob_crossover = 0.6;
 	double n_r = 0.;
-	//cout << "taglia iniziale di pop " << pop.size() << endl;
-	//Individuo i1(this->pop[k]);
-	//Individuo i2(this->pop[l]);
+	
 	vector <Individuo> new_popol;
 	for(int j=0;j<50;j++){
 
@@ -538,9 +415,6 @@ void Popolazione::new_generazione(Random & rnd) {
 			new_popol.push_back(this->pop[l]);
 			new_popol.push_back(this->pop[k]);
 		}
-
-		//cout << "size " << new_popol.size() << endl;
-		//mutazioni su new_popol
 		
 		n_r = rnd.Rannyu();
 		if(n_r < prob_mutazione){
@@ -560,35 +434,18 @@ void Popolazione::new_generazione(Random & rnd) {
       			else new_popol[new_popol.size()-1].mutation_inversion(rnd);
 		}	
 			
-		//cout << "size " << this->pop.size() << endl;
-		//cout << n_r << endl;
-		/*
-		if(n_r < prob_crossover){
-			this->crossover(rnd,k,l);
-			new_popol.push_back(this->pop[this->pop.size()-2]);
-			new_popol.push_back(this->pop[this->pop.size()-1]);
-		}
-		else{
-			new_popol.push_back(this->pop[l]);
-			new_popol.push_back(this->pop[k]);
-		}
-		*/
 	}	
-	//cout <<"ciao" << endl;	
+	
 	this->pop.clear();
-	//cout << "taglia nuova popol " << new_popol.size() << endl;
-
+	
 	for(int i=0; i<100; i++){
 		this->pop.push_back(new_popol[i]);
 	}
 
-	//cout << "taglia nuova " << this->pop.size() << endl;
-	//sort_L1();
-	//cout <<"ciao1" << endl;	
 	new_popol.clear();
-	//cout <<"ciao2" << endl;	
+		
 	rnd.SaveSeed();
-	//cout <<"ciao3" << endl;	
+		
 }
 void Popolazione::migrazione(double * new_x, double * new_y){
 	City inizio(new_x[0],new_y[0]);
@@ -596,7 +453,6 @@ void Popolazione::migrazione(double * new_x, double * new_y){
 	int N=32;
 	for (int i=1; i<N; i++){
 		City c(new_x[i],new_y[i]);
-		//cout << c_q.getx() << " " << c_q.gety() << endl;
 		cities_nuove.push_back(c);
 	}
 	Individuo nuovo(N, inizio, cities_nuove.begin(), cities_nuove.end());
